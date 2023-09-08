3b8d4b9c69c0e616bcfb5ce3e7f8ba5c9a4d64d4
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2023-09-08 19:32:00 +0300

    title-case and palindrome challenges done

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,11 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  const string = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const reversed = string.split("").reverse().join("");
+  console.log(string);
+  if (string === reversed) {
+    return true;
+  }
+  return false;
+}
 
 module.exports = isPalindrome;
