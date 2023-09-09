75b8cfffab76686f3c1963b8305e15000e0b85c0
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2023-09-09 17:56:59 +0300

    Done with basic challenged 1

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,19 @@
-function countVowels() {}
+function countVowels(string) {
+  const lowerCasedString = string.toLowerCase();
+  let counter = 0;
+  for (let i = 0; i < lowerCasedString.length; i++) {
+    const char = lowerCasedString[i];
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      counter++;
+    }
+  }
+  return counter;
+}
 
 module.exports = countVowels;
