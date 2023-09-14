990bf4f1ff4c18473325172299016d8c8a19062d
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2023-09-14 18:34:48 +0300

    Done with 02 03

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,6 @@
-function findMissingNumber() {}
+function findMissingNumber(nums) {
+  const compareNumbers = (a, b) => a - b;
+  const sorted = nums.sort(compareNumbers);
+}
 
 module.exports = findMissingNumber;
